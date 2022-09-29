e125673533e2abd4e171db08385178f71221ee03
tangina7 <113033306+tangina7@users.noreply.github.com>
2022-09-29 13:22:54 +0100

    Add files via upload

---
+++ Brampton Manor/Programming Challenges/Programming challenge 3/test.py
@@ -0,0 +1,12 @@
+import unittest
+
+from The_99_trick import *
+
+class TestPart(unittest.TestCase):
+
+    def test_part1(self):
+        self.assertIsNotNone(2)
+
+
+if __name__ == "__main__":
+    unittest.main()
